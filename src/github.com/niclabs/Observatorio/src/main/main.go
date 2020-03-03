9bc8e1fd4c46713b91d5637c8cc1279f3aada7a2
maitegm <maite.gonzalez.mendoza@gmail.com>
2020-03-03 12:35:27 -0300

    removed max concurrency

--- src/github.com/niclabs/Observatorio/src/main/main.go
+++ src/github.com/niclabs/Observatorio/src/main/main.go
@@ -4,8 +4,6 @@ import(
 	"gopkg.in/yaml.v2"
 	"os"
 	"github.com/niclabs/Observatorio/src/dataCollector"
-	"flag"
-	"github.com/howeyc/gopass"
 	"fmt"
 )
 
@@ -14,7 +12,6 @@ type Config struct {
         Input_filepath string `yaml:"inputfilepath"`
         Dontprobe_filepath string `yaml:"dontprobefilepath"`
         Concurrency int `yaml:"concurrency"`
-        Ccmax int `yaml:"ccmax"`
         Max_retry int `yaml:"maxretry"`
         Drop_database bool `yaml:"dropdatabase"`
         Debug bool `yaml:"debug"`
@@ -50,42 +47,6 @@ func main(){
 	
 
 	dataCollector.InitCollect(cfg.RunArguments.Dontprobe_filepath, cfg.RunArguments.Drop_database, cfg.Database.Username, cfg.Database.Password, cfg.Database.Database_name)
-	dataCollector.Collect(cfg.RunArguments.Input_filepath, cfg.RunArguments.Concurrency, cfg.RunArguments.Ccmax, cfg.RunArguments.Max_retry, cfg.Database.Database_name, cfg.Database.Username, cfg.Database.Password, cfg.RunArguments.Debug)
+	dataCollector.Collect(cfg.RunArguments.Input_filepath, cfg.RunArguments.Concurrency, cfg.RunArguments.Max_retry, cfg.Database.Database_name, cfg.Database.Username, cfg.Database.Password, cfg.RunArguments.Debug)
 }
 
-
-
-
-/*inizialize arguments*/
-func readArguments()(input *string, dp *string, con *int, ccmax int, max_retry *int, dropdatabase *bool, db *string, u *string, pass string, debug *bool){
-	input = flag.String("i", "", "Input file with domains to analize")
-	dp = flag.String("dp", "", "Dont probe file with network to not ask")
-	con = flag.Int("c", 50, "Concurrency: how many routines")
-	cmax := flag.Int("cmax", -1, "max Concurrency: how many routines")
-	max_retry = flag.Int("retry", 1, "retry:how many times")
-	dropdatabase = flag.Bool("drop", false, "true if want to drop database")
-	p := flag.Bool("p", false, "Prompt for password?")
-	u = flag.String("u","","Database User")
-	db = flag.String("db","","Database Name")
-	pw := flag.String("pw","", "Database Password")
-	debug = flag.Bool("d",false,"Debug flag")
-	flag.Parse()
-	pass=""
-	if (*p) {
-		fmt.Printf("Password: ")
-		// Silent. For printing *'s use gopass.GetPasswdMasked()
-		pwd, err := gopass.GetPasswdMasked()
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		pass=string(pwd)
-
-	}else{
-		pass=*pw
-	}
-	ccmax=*cmax
-	if(ccmax==-1){
-		ccmax=*con
-	}
-	return
-}
